39cceb55092a0534e87ac501605e0168f07af23c
Evgeny Shenets <evgenyshenets91@gmail.com>
2020-12-10 00:12:37 +0300

    create separate class ReusableCard with required args in constructor

--- lib/InputPage.dart
+++ lib/InputPage.dart
@@ -12,9 +12,57 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              child: Row(
+            children: <Widget>[
+              Expanded(
+                child: ReusableCard(colour: Color(0xFF1D1E33)),
+              ),
+              Expanded(
+                child: ReusableCard(colour: Color(0xFF1D1E33)),
+              ),
+            ],
+          )),
+          Expanded(
+              child: Row(
+            children: <Widget>[
+              Expanded(
+                child: ReusableCard(colour: Color(0xFF1D1E33)),
+              ),
+            ],
+          )),
+          Expanded(
+              child: Row(
+            children: <Widget>[
+              Expanded(
+                child: ReusableCard(colour: Color(0xFF1D1E33)),
+              ),
+              Expanded(
+                child: ReusableCard(colour: Color(0xFF1D1E33)),
+              ),
+            ],
+          )),
+        ],
       ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.colour});
+
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: colour,
+      ),
+      margin: EdgeInsets.all(15),
     );
   }
 }
